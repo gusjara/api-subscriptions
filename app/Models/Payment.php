d5d5e497a6770c69e85e883c8a83e03c85eb5c41
gus <tavit02@hotmail.com>
2024-02-09 21:26:19 -0300

    agrego controladores para iniciar

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
+    //
+    const GENERATE = 'generado';
+    const SEND_TO_PAY = 'enviado_a_pagar';
+    const PAID = 'pagado';
+
     protected $guarded = [];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'init_date' => 'date',
+        'end_date' => 'date',
+        'period_start' => 'date',
+        'period_end' => 'date',
+        'generate_date' => 'date',
+        'send_to_pay_date' => 'date',
+        'paid_date' => 'date',
+    ];
 }
